9ba08c3398e5e271a4d01ddb765491c0b95e176f
fromwhite <fromwhitebalance@gmail.com>
2023-09-17 15:04:21 +0800

    delete backup appDir

--- bakapp/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="hero loading">
-      <p>loading</p>
-    </div>
-  );
-}
--- bakapp/stitches.config.ts
+++
@@ -1,7 +0,0 @@
-import { createStitches, CSS as StitchesCSS } from "@stitches/react";
-
-export const { config, css, getCssText, globalCss, keyframes, styled, theme } =
-  createStitches();
-
-export type CSS = StitchesCSS<typeof config>;
-export type { VariantProps } from "@stitches/react";
