520a3750af5dde1fc3e62a6ba4efa72c939934b5
fromwhite <fromwhitebalance@gmail.com>
2023-09-20 23:37:02 +0800

    remove globals.css tag:revert,todo:ui system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
-// import "./globals.css";
-// import "normalize.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
