3f7cf2d57503e3c6d109f6f7750ebe6df08de3bc
fromwhite <fromwhitebalance@gmail.com>
2023-09-23 16:39:55 +0800

    local build success,test vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const { withContentlayer } = require("next-contentlayer");
-const withMDX = require("@next/mdx")({
+const { withContentlayer } = require('next-contentlayer')
+const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-});
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+})
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 module.exports = withContentlayer(
   withMDX(
@@ -14,7 +14,7 @@ module.exports = withContentlayer(
         appDir: false,
         mdxRs: true,
       },
-      pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+      pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
       swcMinify: true,
       reactStrictMode: true,
 
@@ -23,17 +23,21 @@ module.exports = withContentlayer(
         // https://github.com/vercel/next.js/issues/25852
         if (isServer) {
           config.output.webassemblyModuleFilename =
-            "./../static/wasm/[modulehash].wasm";
+            './../static/wasm/[modulehash].wasm'
         } else {
           config.output.webassemblyModuleFilename =
-            "static/wasm/[modulehash].wasm";
+            'static/wasm/[modulehash].wasm'
         }
 
         // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-        config.experiments = { asyncWebAssembly: true };
+        /**
+         * 2023/9/23 issue
+         * https://github.com/vercel/next.js/issues/29362
+         */
+        config.experiments = { asyncWebAssembly: true, layers: true }
 
-        return config;
+        return config
       },
     })
   )
-);
+)
