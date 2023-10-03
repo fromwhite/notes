4bcbbf50f9eec21f29407d42d16338a62c3eab24
fromwhite <fromwhitebalance@gmail.com>
2023-10-03 22:39:50 +0800

    chore:remove input autofill

--- components/globalStyle.ts
+++ components/globalStyle.ts
@@ -112,11 +112,6 @@ export const styles = {
     '--code-token-color-15': '#d6deeb',
   },
 
-  'input:-webkit-autofill': {
-    'background-color': 'transparent!important',
-    transition: 'background-color 0s 111111s',
-  },
-
   code: {
     'font-family': 'var(--font-mono)',
   },
