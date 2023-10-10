f0fab6d8404684352105e7393ded393dba78c105
fromwhite <fromwhitebalance@gmail.com>
2023-10-11 00:09:17 +0800

    chore:deploy test

--- next.config.js
+++ next.config.js
@@ -7,6 +7,25 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
+class WasmChunksFixPlugin {
+  apply(compiler) {
+    compiler.hooks.thisCompilation.tap('WasmChunksFixPlugin', (compilation) => {
+      compilation.hooks.processAssets.tap(
+        { name: 'WasmChunksFixPlugin' },
+        (assets) =>
+          Object.entries(assets).forEach(([pathname, source]) => {
+            if (!pathname.match(/\.wasm$/)) return
+            compilation.deleteAsset(pathname)
+
+            const name = pathname.split('/')[1]
+            const info = compilation.assetsInfo.get(pathname)
+            compilation.emitAsset(name, source, info)
+          })
+      )
+    })
+  }
+}
+
 module.exports = withContentlayer(
   withMDX(
     withBundleAnalyzer({
@@ -19,22 +38,27 @@ module.exports = withContentlayer(
       reactStrictMode: true,
 
       // https://github.com/vercel/next.js/issues/25852
-      webpack: function (config, { isServer }) {
-        if (isServer) {
-          config.output.webassemblyModuleFilename =
-            './../static/wasm/[modulehash].wasm'
-        } else {
-          config.output.webassemblyModuleFilename =
-            'static/wasm/[modulehash].wasm'
-        }
-        config.optimization.moduleIds = 'named'
-        config.module.rules.push({
-          test: /\.wasm$/,
-          type: 'webassembly/async',
-        })
+      webpack: function (config, { isServer, dev }) {
+        // if (isServer) {
+        //   config.output.webassemblyModuleFilename =
+        //     './../static/wasm/[modulehash].wasm'
+        // } else {
+        //   config.output.webassemblyModuleFilename =
+        //     'static/wasm/[modulehash].wasm'
+        // }
+        // config.optimization.moduleIds = 'named'
+        // config.module.rules.push({
+        //   test: /\.wasm$/,
+        //   type: 'webassembly/async',
+        // })
         config.experiments = {
           asyncWebAssembly: true,
           layers: true,
+        }
+
+        if (!dev && isServer) {
+          config.output.webassemblyModuleFilename = 'chunks/[id].wasm'
+          config.plugins.push(new WasmChunksFixPlugin())
         }
 
         return config
